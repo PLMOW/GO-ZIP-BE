05675d7f6de1e3b95e00c018d4f558ae0ecdfaf6
SEUNGHYEON HAN <tmdgus980318@gmail.com>
2023-02-24 11:27:25 +0900

    init

---
+++ src/test/java/com/gozip/GozipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gozip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GozipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
